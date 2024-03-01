0304519ccef38ac39ad69f12d877495eeae56bd3
talha12hussain <108652779+talha12hussain@users.noreply.github.com>
2024-02-29 21:00:36 -0800

    Add files via upload

---
+++ reactecommerce/ecomerce/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './component/navbar/Navbar'
+import Hero from './component/Hero/Hero'
+
+const App = () => {
+  return (
+    
+    <div>
+   <Navbar/>
+   <Hero/>
+   </div>
+   
+  )
+}
+
+export default App
